cbb4b467c362f2901023746bb882a5ba3021c8c2
Bartlomiej Swies <bartlomiejswies@gmail.com>
2021-11-17 22:10:57 +0100

    Spring exercise 2

--- src/main/java/pl/bartlomiej_swies/model/TaskRepository.java
+++ src/main/java/pl/bartlomiej_swies/model/TaskRepository.java
@@ -1,23 +1,22 @@
 package pl.bartlomiej_swies.model;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.util.List;
+import java.util.Optional;
 
-@RepositoryRestResource
-public interface TaskRepository extends JpaRepository<Task, Integer> {
+public interface TaskRepository {
+    boolean existsById(Integer id);
 
-    @Override
-    @RestResource(exported = false)
-    void deleteById(Integer integer);
+    List<Task> findAll();
 
-    @Override
-    @RestResource(exported = false)
-    void delete(Task task);
+    Page<Task> findAll(Pageable page);
 
-    @RestResource(path = "done", rel = "done")
     List<Task> findByDone(@Param("state") boolean done);
+
+    Optional<Task> findById(Integer id);
+
+    Task save(Task entity);
 }
