b263a59f660cba66450369e3c032043ae6f4bf8b
Bartlomiej Swies <bartlomiejswies@gmail.com>
2021-12-04 16:58:11 +0100

    Project table added

---
+++ src/main/java/pl/bartlomiej_swies/adapter/SqlProjectRepository.java
@@ -0,0 +1,16 @@
+package pl.bartlomiej_swies.adapter;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import pl.bartlomiej_swies.model.Project;
+import pl.bartlomiej_swies.model.ProjectRepository;
+
+import java.util.List;
+
+@Repository
+public interface SqlProjectRepository extends JpaRepository<Project, Integer>, ProjectRepository {
+    @Override
+    @Query("FROM Project p JOIN FETCH p.steps")
+    List<Project> findAll();
+}
