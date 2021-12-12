6c1e735dc7a53b8dbb13d5c7ba7a3f9014bfbcb0
Bartlomiej Swies <bartlomiejswies@gmail.com>
2021-12-12 22:53:41 +0100

    E2E test

--- src/test/java/pl/bartlomiej_swies/TestConfiguration.java
+++ src/test/java/pl/bartlomiej_swies/TestConfiguration.java
@@ -7,14 +7,27 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import pl.bartlomiej_swies.model.Task;
 import pl.bartlomiej_swies.model.TaskRepository;
 
+import javax.sql.DataSource;
 import java.util.*;
 
 @Configuration
 public class TestConfiguration {
+
     @Bean
+    @Primary
+    @Profile("!integration")
+    DataSource e2eTestDataSource() {
+        var result = new DriverManagerDataSource("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1", "sa", "");
+        result.setDriverClassName("org.h2.Driver");
+        return result;
+    }
+
+    @Bean
+    @Primary
     @Profile("integration")
     TaskRepository testRepository() {
         return new TaskRepository() {
