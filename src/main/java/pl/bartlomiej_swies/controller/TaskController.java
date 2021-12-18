95ac91e12f7751a175653d63c997d96cad3cee16
Bartlomiej Swies <bartlomiejswies@gmail.com>
2021-12-18 12:02:05 +0100

    Compatable future

--- src/main/java/pl/bartlomiej_swies/controller/TaskController.java
+++ src/main/java/pl/bartlomiej_swies/controller/TaskController.java
@@ -2,32 +2,35 @@ package pl.bartlomiej_swies.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+import pl.bartlomiej_swies.logic.TaskService;
 import pl.bartlomiej_swies.model.Task;
 import pl.bartlomiej_swies.model.TaskRepository;
 
 import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("/tasks")
 class TaskController {
     private static final Logger logger = LoggerFactory.getLogger(TaskController.class);
     private final TaskRepository repository;
+    private final TaskService service;
 
-    TaskController(final TaskRepository repository) {
+    TaskController(final TaskRepository repository, final TaskService service) {
         this.repository = repository;
+        this.service = service;
     }
 
     @GetMapping(params = {"!sort", "!page", "!size"})
-    ResponseEntity<List<Task>> readAllTasks() {
+    CompletableFuture<ResponseEntity<List<Task>>> readAllTasks() {
         logger.warn("Exposing all the tasks");
-        return ResponseEntity.ok(repository.findAll());
+        return service.findAllAsync().thenApply(ResponseEntity::ok);
     }
 
     @GetMapping
