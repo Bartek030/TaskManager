4802dc837e7016710188e56a483fc255375bc1c5
Bartlomiej Swies <bartlomiejswies@gmail.com>
2022-01-03 13:58:00 +0100

    Changes counter added

---
+++ src/main/java/pl/bartlomiej_swies/reports/ChangedTaskEventListener.java
@@ -0,0 +1,34 @@
+package pl.bartlomiej_swies.reports;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Service;
+import pl.bartlomiej_swies.model.event.TaskDone;
+import pl.bartlomiej_swies.model.event.TaskEvent;
+import pl.bartlomiej_swies.model.event.TaskUndone;
+
+@Service
+class ChangedTaskEventListener {
+    private static final Logger logger = LoggerFactory.getLogger(ChangedTaskEventListener.class);
+    private final PersistedTaskEventRepository repository;
+
+    ChangedTaskEventListener(final PersistedTaskEventRepository repository) {
+        this.repository = repository;
+    }
+
+    @EventListener
+    public void on(TaskDone event) {
+        onChanged(event);
+    }
+
+    @EventListener
+    public void on(TaskUndone event) {
+        onChanged(event);
+    }
+
+    private void onChanged(final TaskEvent event) {
+        logger.info("Got " + event);
+        repository.save(new PersistedTaskEvent(event));
+    }
+}
