8cd00da5daf86febc94ac1f7d178f09941eb1f9f
Bartlomiej Swies <bartlomiejswies@gmail.com>
2022-01-03 14:02:17 +0100

    Async enabled

--- src/main/java/pl/bartlomiej_swies/reports/ChangedTaskEventListener.java
+++ src/main/java/pl/bartlomiej_swies/reports/ChangedTaskEventListener.java
@@ -3,6 +3,7 @@ package pl.bartlomiej_swies.reports;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import pl.bartlomiej_swies.model.event.TaskDone;
 import pl.bartlomiej_swies.model.event.TaskEvent;
@@ -17,11 +18,13 @@ class ChangedTaskEventListener {
         this.repository = repository;
     }
 
+    @Async
     @EventListener
     public void on(TaskDone event) {
         onChanged(event);
     }
 
+    @Async
     @EventListener
     public void on(TaskUndone event) {
         onChanged(event);
